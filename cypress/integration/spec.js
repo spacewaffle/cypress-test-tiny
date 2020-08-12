2e87c4528ad33d42d356391995b41dda96e4a824
Jon Cheng <jon.syndicate@gmail.com>
2020-08-12 18:36:35 -0400

    working example

--- cypress/integration/spec.js
+++ cypress/integration/spec.js
@@ -1,6 +1,20 @@
 /// <reference types="cypress" />
 describe('page', () => {
-  it('works', () => {
-    cy.visit('https://example.cypress.io')
+  beforeEach(function () {
+    cy.visit('/index.html', {
+      onBeforeLoad(window) {
+        cy.stub(window, "open").as("windowOpen")
+        cy.stub(window.location, "reload").as("locationReload")
+        // cy.stub(window, "location.reload").as('locationReload')
+      }
+    })
+  })
+
+  it('stubs window.open', () => {
+    cy.get("#openButton").click();
+  })
+
+  it("doesn't reload the page", () => {
+    cy.get("#reloadButton").click()
   })
 })
